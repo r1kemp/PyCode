88932b314dbc39e5a7b8d3b0f408b68f0042b2ef
r1kemp <ravi.kemp@live.com>
2021-12-19 23:40:00 -0800

    simplified generator sample

--- yield_fib.py
+++ yield_fib.py
@@ -1,26 +1,18 @@
 # yield_fib
 
-cube = lambda x: x**3
 
-def fibonacci_yield(n):
-    if n == 0:
-        return
-
-    yield 0
-    if n > 1:
-        yield 1
-    a, b, m = 0, 1, 2
-    while m < n:
-        m += 1
+def fib_yield(n):
+    a, b = 0, 1
+    while n > 0:
+        yield a
+        n -= 1
         a, b = b, a + b
-        yield b
-    
-def fibonacci(n):
-    A = [0, 1]
-    for x in range(2, n): A.append(A[x-1] + A[x-2])
-    return A[:n]
 
 if __name__ == '__main__':
-    n = int(input())
-    print(list(map(cube, fibonacci(n))))
+    n = int(input("N = "))
+    fib = fib_yield(n)
+    print(fib)
+    for x in fib:
+        print(x, end=' ')
+    print("")
     
